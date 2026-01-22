07d0caf71d1f2e901b2a2e7fcbe74ccb9b407921
jean vicelli <jcvicelli@gmail.com>
2026-01-22 17:48:18 +0100

    change secrets logic

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,14 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35.0"
+    }
   }
 }
 
@@ -19,6 +27,31 @@ provider "aws" {
     tags = {
       Project   = "Wiz-Technical-Exercise"
       ManagedBy = "Terraform"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
